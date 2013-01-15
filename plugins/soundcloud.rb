2cbe685209b11350a12972e0fc52bd748ef6c40a
Mark McCoy <mark.mccoy@utsa.edu>
2013-01-15 17:28:30 -0600

    adjusted height of soundcloud player

--- plugins/soundcloud.rb
+++ plugins/soundcloud.rb
@@ -71,7 +71,7 @@ module Jekyll
 
     def render(context)
       if @type and @id
-        @height = (450 unless @type == 'tracks') || 166
+        @height = (380 unless @type == 'tracks') || 166
         @resource = (@type unless @type === 'favorites') || 'users'
         @extra = ("" unless @type === 'favorites') || '%2Ffavorites'
         @joined_options = @options.join("&amp;")
